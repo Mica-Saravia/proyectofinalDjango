e6ac7bf4db20056a3c7da2aecd15f35e09971e25
Virginia <128263468+Virtar@users.noreply.github.com>
2024-07-19 11:39:49 -0300

    Delete EntornoVirtual/movies/tendencias/borrar_ej2_views.py

--- EntornoVirtual/movies/tendencias/borrar_ej2_views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def infoutil(request):
-    infoutils = InfoUtil.objects.all()
-    return render(request, "infoutil/infoutil.html", {'infoutils':infoutils})
