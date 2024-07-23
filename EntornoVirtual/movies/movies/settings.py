5781a4e857b8e89df39337078fea471571d50424
Mica-Saravia <micaela.saraviia@gmail.com>
2024-07-23 00:25:36 -0300

    Agregando ruta estatica y Borrando secret key

--- EntornoVirtual/movies/movies/settings.py
+++ EntornoVirtual/movies/movies/settings.py
@@ -20,11 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bquk^33f0_yeeg(cls&b8av!yqv7%ef&f#i$l^yqmm2#ivo4n3'
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -123,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # archivos media
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,"media")
